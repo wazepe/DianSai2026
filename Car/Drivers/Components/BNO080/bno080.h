c3314e98f2455d0916d596d0a469045a2cf07437
wazepe <wan230808@163.com>
2026-07-23 20:34:59 +0800

    陈佳宇要求

--- Car/Drivers/Components/BNO080/bno080.h
+++ Car/Drivers/Components/BNO080/bno080.h
@@ -1,20 +1,3 @@
-/*
- * SysConfig Configuration Steps:
- *   UART:
- *     1. Add an UART module.
- *     2. Name it as "UART_BNO08X".
- *     3. Set "Target Baud Rate" to "115200".
- *     4. Set "Communication Direction " to "RX only".
- *     5. Check the box "Enable FIFOs".
- *     6. Set "RX Timeout Interrupt Counts" to "1".
- *     7. Check the "RX timeout" box at "Enable Interrupts".
- *     8. Set "Configure DMA RX Trigger" to "UART RX interrupt".
- *     9. Set "DMA Channel RX Name" to "DMA_BNO08X".
- *     10. Set "Address Mode" to "Fixed addr. to Block addr.".
- *     11. Set "Source Length" and "Destination Length" to "Byte".
- *     12. Set the pin according to your needs.
- */
-
 #ifndef _BNO08X_UART_RVC_H_
 #define _BNO08X_UART_RVC_H_
 
